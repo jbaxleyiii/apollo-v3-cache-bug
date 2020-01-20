d461c49ad4a3a816bc3c7d483a5e15df3eea1b2a
jbaxleyiii <james@apollographql.com>
2020-01-19 19:01:47 -0500

    Add merge fn for  field

--- apollo.test.js
+++ apollo.test.js
@@ -1,73 +1,90 @@
-const gql = require('graphql-tag').default;
-const fetchMock = require('fetch-mock');
-const LegacyApolloClient = require('apollo-client').ApolloClient;
-const LegacyApolloCache = require('apollo-cache-inmemory').InMemoryCache;
-const LegacyApolloHttpLink = require('apollo-link-http').HttpLink;
-const NewApolloClient = require('@apollo/client').ApolloClient;
-const NewApolloCache = require('@apollo/client').InMemoryCache;
-const NewApolloHttpLink = require('@apollo/client').HttpLink;
+const gql = require("graphql-tag").default;
+const fetchMock = require("fetch-mock");
+const LegacyApolloClient = require("apollo-client").ApolloClient;
+const LegacyApolloCache = require("apollo-cache-inmemory").InMemoryCache;
+const LegacyApolloHttpLink = require("apollo-link-http").HttpLink;
+const NewApolloClient = require("@apollo/client").ApolloClient;
+const NewApolloCache = require("@apollo/client").InMemoryCache;
+const NewApolloHttpLink = require("@apollo/client").HttpLink;
 
-const BarQuery = gql`query BarQuery {
-  foo {
-    bar
+const BarQuery = gql`
+  query BarQuery {
+    foo {
+      bar
+    }
   }
-}`
+`;
 
 const mockBarResponse = {
   foo: {
-    __typename: 'FooQuery',
+    __typename: "FooQuery",
     bar: {
-      __typename: 'Bar',
+      __typename: "Bar",
       value: 1
     }
   }
 };
 
-const BazQuery = gql`query BarQuery {
-  foo {
-    baz
+const BazQuery = gql`
+  query BarQuery {
+    foo {
+      baz
+    }
   }
-}`
+`;
 
 const mockBazResponse = {
   foo: {
-    __typename: 'FooQuery',
+    __typename: "FooQuery",
     baz: {
-      __typename: 'Bar',
+      __typename: "Bar",
       value: 1
     }
   }
 };
-fetchMock
-  .post('/graphql', (url, { body }) => {
-    if(/bar/.test(body))
-      return { body: { data: mockBarResponse } };
-    else if (/baz/.test(body))
-      return { body: { data: mockBazResponse } };
-    else
-      throw new Error('No matching route');
-  })
+fetchMock.post("/graphql", (url, { body }) => {
+  if (/bar/.test(body)) return { body: { data: mockBarResponse } };
+  else if (/baz/.test(body)) return { body: { data: mockBazResponse } };
+  else throw new Error("No matching route");
+});
 
 describe.each([
-  ['Legacy', LegacyApolloClient, LegacyApolloCache, LegacyApolloHttpLink],
-  ['New', NewApolloClient, NewApolloCache, NewApolloHttpLink],
-])('%s client', (_name, Client, Cache, HttpLink) => {
-
-  it('can make two request and cache both', async () => {
+  ["Legacy", LegacyApolloClient, LegacyApolloCache, LegacyApolloHttpLink],
+  ["New", NewApolloClient, NewApolloCache, NewApolloHttpLink]
+])("%s client", (_name, Client, Cache, HttpLink) => {
+  it("can make two request and cache both", async () => {
     let client = new Client({
       link: new HttpLink({
-        uri: '/graphql',
+        uri: "/graphql"
       }),
-      cache: new Cache(),
+      cache: new Cache({
+        typePolicies: {
+          Query: {
+            fields: {
+              foo: {
+                merge(a, b) {
+                  return { ...a, ...b };
+                }
+              }
+            }
+          }
+        }
+      })
     });
 
-    const barResult = await client.query({ query: BarQuery, } );
-    expect(barResult .data).toEqual(mockBarResponse);
-    expect(client.cache.readQuery({ query: BarQuery })).toEqual(mockBarResponse);
+    const barResult = await client.query({ query: BarQuery });
+    expect(barResult.data).toEqual(mockBarResponse);
+    expect(client.cache.readQuery({ query: BarQuery })).toEqual(
+      mockBarResponse
+    );
 
-    const bazResult = await client.query({ query: BazQuery } );
+    const bazResult = await client.query({ query: BazQuery });
     expect(bazResult.data).toEqual(mockBazResponse);
-    expect(client.cache.readQuery({ query: BazQuery })).toEqual(mockBazResponse);
-    expect(client.cache.readQuery({ query: BarQuery })).toEqual(mockBarResponse);
+    expect(client.cache.readQuery({ query: BazQuery })).toEqual(
+      mockBazResponse
+    );
+    expect(client.cache.readQuery({ query: BarQuery })).toEqual(
+      mockBarResponse
+    );
   });
 });
